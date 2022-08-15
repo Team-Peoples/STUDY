9a40612c57747f4cba2de176c198b5bf8712e680
domb <woon4910@gmail.com>
2022-08-15 13:24:53 +0900

    feat: add CustomAF.swift for Networking, add Networking Method in Network.swift

---
+++ STUDYA/STUDYA/CustomAF.swift
@@ -0,0 +1,208 @@
+//
+//  CustomAF.swift
+//  STUDYA
+//
+//  Created by 서동운 on 2022/08/15.
+//
+
+import Foundation
+import Alamofire
+
+protocol Requestable: URLRequestConvertible {
+    var baseUrl: String { get }
+    var path: String { get }
+    var parameters: RequestParameters { get }
+}
+
+enum RequestPurpose: Requestable {
+    case signIn(Credential)
+    case signUp(User?)
+    case emailCheck(Id)
+    case create(Object)
+    case issued
+    
+    case nicknameChange(String) // nickname
+    case profileImageChange(Id)
+    case scheduleStatusChange(String) // study Id
+    
+    case delete(Id)
+    
+    case getStudy(Id)
+    case getAllSchedule
+    case getUserSchedule
+    case getStudyLog
+}
+
+extension RequestPurpose {
+    var baseUrl: String {
+        return "http://localhost:8082/api/v1"
+    }
+    
+    var path: String {
+        switch self {
+            case .signUp:
+                return "/signup"
+            case .emailCheck:
+                return "/signup/verification"
+            case .signIn:
+                return "/signin"
+            case .create(let object):
+                if type(of: object) == Study.self {
+                    return "/study"
+                } else {
+                    return  "/user/schedule"
+                }
+            case .issued:
+                return "/issued"
+                
+            case .nicknameChange:
+                return "/nickname"
+            case .profileImageChange:
+                return "/user/profile/img"
+            case .scheduleStatusChange(let studyId):
+                return "/user/schedule/\(studyId)"
+            
+            case .delete(let id):
+                guard let id = id else { return "/user/"}
+                return "/user/\(id)"
+                
+            case .getStudy(let id):
+                guard let id = id else { return "/study"}
+                return "/study/\(id)"
+            case .getStudyLog:
+                return "/user/history"
+            case .getAllSchedule:
+                return "/study/schedule"
+            case .getUserSchedule:
+                return "/user/schedule"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+            case .signIn, .signUp, .emailCheck,.create, .issued: return .post
+                
+            case .nicknameChange, .profileImageChange, .scheduleStatusChange: return .put
+                
+            case .delete(_): return .delete
+                
+            case .getStudy(_), .getStudyLog, .getAllSchedule, .getUserSchedule: return .get
+        }
+    }
+    
+    var parameters: RequestParameters {
+        switch self {
+            case .signIn(let credential):
+                return .body(credential)
+            case .signUp(let user):
+                return .body(user)
+            case .emailCheck(let email):
+                return .body(["userId": email])
+            case .create(let object):
+                return .body(object)
+            case .issued:
+                return .none
+                
+            case .nicknameChange(let nickName):
+                return .body(["nickname": nickName])
+            case .profileImageChange(let email):
+                return .body(["userId": email])
+            case .scheduleStatusChange:
+                 return .none
+                
+            case .delete:
+                return .none
+            default:
+                return .none
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = try baseUrl.asURL()
+        var urlRequest = try URLRequest(url: url.appendingPathComponent(path), method: method)
+        
+        switch self {
+            case .signUp(_), .profileImageChange(_):
+                urlRequest.headers.add(HTTPHeader.contentType("multipartFormData"))
+            default:
+                urlRequest.headers.add(HTTPHeader.contentType("application/json"))
+        }
+        
+        switch parameters {
+            case .body(let parameter):
+                let jsonParameter = parameter.toJSONData()
+                urlRequest.httpBody = jsonParameter
+                return urlRequest
+            case .none:
+                return urlRequest
+        }
+    }
+}
+
+enum RequestParameters {
+    case body(_ parameter: Encodable)
+    case none
+}
+
+struct TokenRequestInterceptor: RequestInterceptor {
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+            completion(.success(urlRequest))
+        let accessToken = ""
+        let refreshToken = ""
+        var request = urlRequest
+        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "AccessToken")
+        request.addValue("Bearer \(refreshToken)", forHTTPHeaderField: "RefreshToken")
+
+        completion(.success(request))
+    }
+}
+// MARK: - Helpers
+
+extension Encodable {
+    func toJSONData() -> Data? {
+        let jsonData = try? JSONEncoder().encode(self)
+        return jsonData
+    }
+}
+
+extension Data {
+    func toDictionary() -> [String: Any] {
+        guard let dictionaryData = try? JSONSerialization.jsonObject(with: self) as? [String: Any] else { return [:] }
+        return dictionaryData
+    }
+}
+
+// MARK: - Model
+///네트워크에서 쓰이는 모델
+///
+protocol Object: Encodable {
+    
+}
+
+struct Study: Object  {
+    
+}
+
+struct Schedule: Object {
+    
+}
+
+struct UserProfile: Codable {
+    let nickname: String
+    let img: Data
+}
+
+struct User: Codable {
+    let userId: String
+    let password: String?
+    let password_check: String?
+    let nickname: String?
+}
+
+struct Credential: Encodable {
+    let userId: String
+    let password: String?
+}
+
+typealias Id = String? //스터디 아이디 또는 사용자의 아이디
+
--- STUDYA/STUDYA/Network.swift
+++ STUDYA/STUDYA/Network.swift
@@ -5,8 +5,49 @@
 //  Created by 신동훈 on 2022/08/14.
 //
 
-import Foundation
+import UIKit
+import Alamofire
 
 struct Network {
+    static let shared = Network()
     
+    func signUp(user: User, imageData: UIImage) {
+        let imageData = imageData.jpegData(compressionQuality: 1)!
+        AF.upload(multipartFormData: { data in
+            data.append(imageData, withName: "profile_Image", fileName: nil, mimeType: nil)
+            
+        }, with: RequestPurpose.signUp(user)).validate().responseData { response in
+            switch response.result {
+                case .success(let data):
+                    let response = data.toDictionary()
+                    print(response)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+    
+    func signIn(credential: Credential) {
+        AF.request(RequestPurpose.signIn(credential)).validate().responseData { response in
+            switch response.result {
+                case .success(let data):
+                    let response = data.toDictionary()
+                    print(response)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+    
+    func check(email: String) {
+        AF.request(RequestPurpose.emailCheck(email)).validate().responseData { response in
+            switch response.result {
+                case .success(let data):
+                    let response = data.toDictionary()
+                    print(response)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
 }
