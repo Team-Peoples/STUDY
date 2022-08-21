d048d584364392ec76b47444720273046bd6da9c
ThisidEhd <sem789456@gmail.com>
2022-08-21 17:22:02 +0900

    feat: MailCheckVC docs: add emailCheck image chore: add whiteLabel (=white) to AssetColor

--- STUDYA/STUDYA/Color.swift
+++ STUDYA/STUDYA/Color.swift
@@ -8,39 +8,41 @@
 import UIKit
 
 enum AssetColor: String {
-    case brandLight, brandMedium, brandDark, descriptionGeneral, subTitleGeneral, titleGeneral, background, highlightDeep, highlightMedium, highlightLight, kakao, kakaoBrown, naver
+    case brandLight, brandMedium, brandDark, descriptionGeneral, subTitleGeneral, titleGeneral, background, highlightDeep, highlightMedium, highlightLight, kakao, kakaoBrown, naver, whiteLabel
 }
 
 extension UIColor {
     
     static func appColor(_ name: AssetColor) -> UIColor {
         switch name {
-            case .brandLight:
-                return UIColor(named: AssetColor.brandLight.rawValue)!
-            case .brandMedium:
-                return UIColor(named: AssetColor.brandMedium.rawValue)!
-            case .brandDark:
-                return UIColor(named: AssetColor.brandDark.rawValue)!
-            case .descriptionGeneral:
-                return UIColor(named: AssetColor.descriptionGeneral.rawValue)!
-            case .subTitleGeneral:
-                return UIColor(named: AssetColor.subTitleGeneral.rawValue)!
-            case .titleGeneral:
-                return UIColor(named: AssetColor.titleGeneral.rawValue)!
-            case .background:
-                return UIColor(named: AssetColor.background.rawValue)!
-            case .highlightDeep:
-                return UIColor(named: AssetColor.highlightDeep.rawValue)!
-            case .highlightMedium:
-                return UIColor(named: AssetColor.highlightMedium.rawValue)!
-            case .highlightLight:
-                return UIColor(named: AssetColor.highlightLight.rawValue)!
-            case .kakao:
-                return UIColor(red: 254/255, green: 229/255, blue: 0, alpha: 1)
-            case .kakaoBrown:
-                return UIColor(red: 60/255, green: 30/255, blue: 30/255, alpha: 1)
-            case .naver:
-                return UIColor(red: 3/255, green: 199/255, blue: 90/255, alpha: 1)
+        case .brandLight:
+            return UIColor(named: AssetColor.brandLight.rawValue)!
+        case .brandMedium:
+            return UIColor(named: AssetColor.brandMedium.rawValue)!
+        case .brandDark:
+            return UIColor(named: AssetColor.brandDark.rawValue)!
+        case .descriptionGeneral:
+            return UIColor(named: AssetColor.descriptionGeneral.rawValue)!
+        case .subTitleGeneral:
+            return UIColor(named: AssetColor.subTitleGeneral.rawValue)!
+        case .titleGeneral:
+            return UIColor(named: AssetColor.titleGeneral.rawValue)!
+        case .background:
+            return UIColor(named: AssetColor.background.rawValue)!
+        case .highlightDeep:
+            return UIColor(named: AssetColor.highlightDeep.rawValue)!
+        case .highlightMedium:
+            return UIColor(named: AssetColor.highlightMedium.rawValue)!
+        case .highlightLight:
+            return UIColor(named: AssetColor.highlightLight.rawValue)!
+        case .kakao:
+            return UIColor(red: 254/255, green: 229/255, blue: 0, alpha: 1)
+        case .kakaoBrown:
+            return UIColor(red: 60/255, green: 30/255, blue: 30/255, alpha: 1)
+        case .naver:
+            return UIColor(red: 3/255, green: 199/255, blue: 90/255, alpha: 1)
+        case .whiteLabel:
+            return white
         }
     }
 }
