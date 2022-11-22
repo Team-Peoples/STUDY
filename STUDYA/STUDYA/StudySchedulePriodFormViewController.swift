54184c3642bca6786de5262ff6dc333491c22a3e
domb <woon4910@gmail.com>
2022-11-22 19:48:53 +0900

    feat: Add ViewControllers and related Assets

---
+++ STUDYA/STUDYA/StudySchedulePriodFormViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudyScheduleFormViewController.swift
+//  STUDYA
+//
+//  Created by 서동운 on 11/22/22.
+//
+
+import UIKit
+
+class StudySchedulePriodFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ STUDYA/STUDYA/StudyScheduleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudyScheduleViewController.swift
+//  STUDYA
+//
+//  Created by EHDDOMB on 11/22/22.
+//
+
+import UIKit
+
+class StudyScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
