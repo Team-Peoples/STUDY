2be7f6e50ebd188f7527f473a6ebf502df140119
domb <woon4910@gmail.com>
2023-03-06 09:59:00 +0900

    fix: Resolve issues that were not updated after Editing study info

--- STUDYA/STUDYA/Resource/Notification.swift
+++ STUDYA/STUDYA/Resource/Notification.swift
@@ -25,4 +25,5 @@ extension Notification.Name {
     static let calendarShouldUpdate = Notification.Name(rawValue: "calendarShouldUpdate")
     static let reloadScheduleTableView = Notification.Name(rawValue: "reloadScheduleTableView")
     static let bottomSheetSizeChanged = Notification.Name(rawValue: "bottomSheetSizeChanged")
+    static let studyInfoShouldUpdate = Notification.Name(rawValue: "studyInfoShouldUpdate")
 }
