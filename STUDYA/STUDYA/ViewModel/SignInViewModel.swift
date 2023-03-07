f215061bb8f77a9356865b2627c012908cc74ccd
domb <woon4910@gmail.com>
2023-03-07 14:44:11 +0900

    feat: separate every ViewModel Object  in a ViewModel.swift

---
+++ STUDYA/STUDYA/ViewModel/SignInViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  SignInViewModel.swift
+//  STUDYA
+//
+//  Created by 서동운 on 3/6/23.
+//
+
+import Foundation
+
+// MARK: - SignInViewModel
+
+class SignInViewModel: ViewModel {
+    typealias Model = Credential
+    
+    var credential: Credential {
+        didSet {
+            handler?(credential)
+        }
+    }
+    var handler: DataHandler?
+    
+    init(credential: Credential = Credential()) {
+        self.credential = credential
+    }
+    
+    func singIn(_ successHandler: @escaping (User) -> Void, _ failureHandler: @escaping (PeoplesError) -> Void) {
+        guard let email = credential.email, let password = credential.password else { return }
+        
+        Network.shared.signIn(id: email, pw: password) { result in
+            switch result {
+            case .success(let user):
+                
+                successHandler(user)
+            case .failure(let error):
+                
+                failureHandler(error)
+            }
+        }
+    }
+}
