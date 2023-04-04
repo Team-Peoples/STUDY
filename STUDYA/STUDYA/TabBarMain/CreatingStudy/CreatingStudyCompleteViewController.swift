dac77d23978e7c9f90cca42644e8918b18ae90a2
ThisidEhd <sem789456@gmail.com>
2023-04-04 23:59:52 +0900

    refactor: create MainViewModel

--- STUDYA/STUDYA/TabBarMain/CreatingStudy/CreatingStudyCompleteViewController.swift
+++ STUDYA/STUDYA/TabBarMain/CreatingStudy/CreatingStudyCompleteViewController.swift
@@ -11,11 +11,22 @@ final class CreatingStudyCompleteViewController: UIViewController {
     
     // MARK: - Properties
     
+    var studyID: ID
+    
     private let titleLabel = CustomLabel(title: "스터디를 만들었어요!", tintColor: .ppsBlack, size: 28, isBold: true)
     private let completeImageView = UIImageView(image: UIImage(named: "congratulation"))
     private let completeButton = BrandButton(title: Constant.done, isBold: true, isFill: true)
     
     // MARK: - Life Cycle
+    
+    init(studyID: ID) {
+        self.studyID = studyID
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +48,7 @@ final class CreatingStudyCompleteViewController: UIViewController {
         guard let mainNavigationController = tabbarController.selectedViewController as? UINavigationController else { return }
         guard let mainVC = mainNavigationController.viewControllers.first as? MainViewController else { return }
         
-        NotificationCenter.default.post(name: .reloadStudyList, object: nil)
+        NotificationCenter.default.post(name: .reloadStudyList, object: nil, userInfo: [Constant.studyID: studyID])
         
         dismiss(animated: true) {
             let linkShareMessageView = MainLinkShareMessageView()
